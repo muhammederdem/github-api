0bed2497c9c7fd31611bcb4b2ded5c9f2b5b4ff0
Muhammed Erdem <iamuhammederdem@gmail.com>
2019-10-10 16:58:58 +0300

    initial commit

---
+++ src/components/Preloader.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Preloader extends Component {
+  render() {
+    return (
+      <div className="preloader">
+        <h1>Loading</h1>
+      </div>
+    );
+  }
+}
+
+export default Preloader;
